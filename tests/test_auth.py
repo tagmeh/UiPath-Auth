c09c2737060b735d393c51da688468230970dca1
John <tagmeh@gmail.com>
2020-03-02 21:20:45 -0600

    Updated test_cloud_auth with new orchestrator requirement.

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -8,21 +8,22 @@ ROOT = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 
 class TestAuth(unittest.TestCase):
 
-	def setUp(self) -> None:
-		self.config = configparser.ConfigParser()
-		self.config.read(os.path.join(ROOT, r'files\creds.ini'))
+    def setUp(self) -> None:
+        self.config = configparser.ConfigParser()
+        self.config.read(os.path.join(ROOT, r'files\creds.ini'))
 
-	def test_cloud_auth(self):
-		"""
-		First iteration assumes there's a uipath_api/files/creds.ini file with the below section and keys.
-		Tests the Cloud.authenticate method.
-		"""
-		cloud = auth.Cloud(
-			user_key=self.config.get('CLOUD', 'user_key'),
-			client_id=self.config.get('CLOUD', 'client_id'),
-			tenant_logical_name=self.config.get('CLOUD', 'tenant_logical_name'),
-			account_logical_name=self.config.get('CLOUD', 'account_logical_name'),
-		)
+    def test_cloud_auth(self):
+        """
+        First iteration assumes there's a uipath_api/files/creds.ini file with the below section and keys.
+        Tests the Cloud.authenticate method.
+        """
+        cloud = auth.Cloud(
+            orchestrator='',
+            user_key=self.config.get('CLOUD', 'user_key'),
+            client_id=self.config.get('CLOUD', 'client_id'),
+            tenant_logical_name=self.config.get('CLOUD', 'tenant_logical_name'),
+            account_logical_name=self.config.get('CLOUD', 'account_logical_name'),
+        )
 
-		response = cloud.authenticate()  # Authenticates the session
-		assert response.status_code == 200
+        response = cloud.authenticate()  # Authenticates the session
+        assert response.status_code == 200
